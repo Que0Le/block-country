e2a24c50636fcd80d875657a401f3b65403858fa
tuilagio <ba.q.le@campus.tu-berlin.de>
2019-04-27 13:43:53 +0200

    4

--- block-country.sh
+++ block-country.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #Script to process ip ranges to ban using IPSet and IPTables
 # https://www.ip2location.com/free/visitor-blocker
+# https://www.itworld.com/article/2833357/how-to-block-traffic-from-other-countries-in-linux.html
 ipset create countryblock hash:net
 while read line; do ipset add countryblock $line; done < china.txt
 iptables -I INPUT -m set --match-set countryblock src -j DROP
