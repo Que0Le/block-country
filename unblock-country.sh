67bb50e167e0f3dcc7c7b103355b40cbe73f5274
h1 <h1@h1s-iMac.local>
2019-05-15 22:48:10 +0200

    3

--- unblock-country.sh
+++ unblock-country.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 # https://scottlinux.com/2013/08/30/block-geo-region-list-of-ips-with-ufw-in-linux/
-while read line; do sudo ufw delete deny from $line to any; done < test*.txt
-
+#while read line; do sudo ufw delete deny from $line to any; done < test*.txt
+#while read line; do sudo ufw delete deny from $line to any; done < brazil*.txt
+#while read line; do sudo ufw delete deny from $line to any; done < china*.txt
+#while read line; do sudo ufw delete deny from $line to any; done < india*.txt
+#while read line; do sudo ufw delete deny from $line to any; done < indo*.txt
+#while read line; do sudo ufw delete deny from $line to any; done < phil*.txt
+#while read line; do sudo ufw delete deny from $line to any; done < russian*.txt
+while read line; do sudo ufw delete deny from $line to any; done < turkey*.txt
 
 
 #Script to process ip ranges to ban using IPSet and IPTables
