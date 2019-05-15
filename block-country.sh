3ea73a4d1fe4cac5ee0f83b3b01d9ca3d888f5ac
h1 <h1@h1s-iMac.local>
2019-05-15 22:25:08 +0200

    1

--- block-country.sh
+++ block-country.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+# https://scottlinux.com/2013/08/30/block-geo-region-list-of-ips-with-ufw-in-linux/
+while read line; do sudo ufw insert 1 deny from $line to any; done < test*.txt
+
+
+
 #Script to process ip ranges to ban using IPSet and IPTables
 # https://www.ip2location.com/free/visitor-blocker
 # https://www.itworld.com/article/2833357/how-to-block-traffic-from-other-countries-in-linux.html
-ipset create countryblock hash:net
-while read line; do ipset add countryblock $line; done < china.txt
-iptables -I INPUT -m set --match-set countryblock src -j DROP
+#ipset create countryblock hash:net
+#while read line; do ipset add countryblock $line; done < china.txt
+#iptables -I INPUT -m set --match-set countryblock src -j DROP
