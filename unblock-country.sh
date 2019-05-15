6eb5e8ec7e313740d9bea817e094f2da239b4672
h1 <h1@h1s-iMac.local>
2019-05-15 23:16:21 +0200

    4

--- unblock-country.sh
+++ unblock-country.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # https://scottlinux.com/2013/08/30/block-geo-region-list-of-ips-with-ufw-in-linux/
 #while read line; do sudo ufw delete deny from $line to any; done < test*.txt
-#while read line; do sudo ufw delete deny from $line to any; done < brazil*.txt
-#while read line; do sudo ufw delete deny from $line to any; done < china*.txt
-#while read line; do sudo ufw delete deny from $line to any; done < india*.txt
-#while read line; do sudo ufw delete deny from $line to any; done < indo*.txt
-#while read line; do sudo ufw delete deny from $line to any; done < phil*.txt
-#while read line; do sudo ufw delete deny from $line to any; done < russian*.txt
+while read line; do sudo ufw delete deny from $line to any; done < brazil*.txt
+while read line; do sudo ufw delete deny from $line to any; done < china*.txt
+while read line; do sudo ufw delete deny from $line to any; done < india*.txt
+while read line; do sudo ufw delete deny from $line to any; done < indo*.txt
+while read line; do sudo ufw delete deny from $line to any; done < phil*.txt
+while read line; do sudo ufw delete deny from $line to any; done < russian*.txt
 while read line; do sudo ufw delete deny from $line to any; done < turkey*.txt
 
 
