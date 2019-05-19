499fb7ed66dba500bfae823aedd9dbb575f8bb9a
h1 <h1@h1s-iMac.local>
2019-05-19 11:16:10 +0200

    3

--- block-list.sh
+++ block-list.sh
@@ -1,3 +1,3 @@
-while read line; do sudo ufw deny from $line to any; done < firehol_level1*.netset
-while read line; do sudo ufw deny from $line to any; done < firehol_level1*.netset
-while read line; do sudo ufw deny from $line to any; done < firehol_level1*.netset
+while read line; do sudo ufw deny from $line to any; done < firehol_level1_19_05_19.netset
+while read line; do sudo ufw deny from $line to any; done < firehol_level1_19_05_19.netset
+while read line; do sudo ufw deny from $line to any; done < firehol_level1_19_05_19.netset
